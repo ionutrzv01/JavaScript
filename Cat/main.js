a6b38a45a41d45d0e2da5908b1aac8a5b97b742a
ionutrzv01 <ionutrazvan01@yahoo.com>
2019-01-24 22:24:08 +0200

    My first commit

--- Cat/main.js
+++ Cat/main.js
@@ -3,23 +3,30 @@ const card = document.getElementById("poza");
 var img = document.createElement("img");
 card.appendChild(img);
 
-
-
+function generateImage(x){
+    img.src=x;
+}
+generateImage("https://www.kimballstock.com/pix/COU/01/COU_01_DB0012_01_P.JPG");
 
 
 function lista(pisica){
     for(i=0;i < pisica.length; i++){
         
         var lista = document.createElement('option');
-        lista.response=pisica[i].name;
+        lista.value=pisica[i].id;
         lista.innerText=pisica[i].name;
+      
+       
+        
+        
+
         select.appendChild(lista);
-        console.log(pisica[i].name);
+        
     }
 }
 
 
-var newVar=fetch('https://api.thecatapi.com/v1/breeds')
+var newVar=fetch("https://api.thecatapi.com/v1/breeds")
     .then( (response) => response.json() )
     .then( (data) => lista(data) );                                 ///Rase pisici
 
@@ -27,5 +34,15 @@ var newVar=fetch('https://api.thecatapi.com/v1/breeds')
 
     ///poza
     
-    
-img="https://www.kimballstock.com/pix/COU/01/COU_01_DB0012_01_P.JPG";
+const buton=document.getElementById("buton");
+
+buton.addEventListener("click",(func)=>{
+
+fetch("https://api.thecatapi.com/v1/images/search?breed_id=" + select.value)
+.then(res=>res.json())
+.then(data=>generateImage(data.message));
+
+}
+
+
+);
