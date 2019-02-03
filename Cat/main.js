80634d4a2d4b833fbf5a5d1d57a92acaec824836
ionutrzv01 <ionutrazvan01@yahoo.com>
2019-02-03 20:49:14 +0200

    .

--- Cat/main.js
+++ Cat/main.js
@@ -1,5 +1,5 @@
 const buton = document.getElementById("buton");
-const select = document.getElementById('select');
+const select = document.getElementById("select");
 const card = document.getElementById("poza");
 const poz = document.getElementById("poz");
 const poz1 = document.getElementById("poz1");
@@ -13,12 +13,30 @@ const buton7 = document.getElementById("ties");
 const buton8 = document.getElementById("gif");
 const pisicute = document.getElementById("pisicute");
 var img = document.createElement("img");
-const butoane = [buton1, buton2, buton3, buton4, buton5, buton6, buton7, buton8];
-const login = { headers: { "x-api-key": "7c59bf11-7fe0-4615-9ba0-327d59f34266" } };
-const adrese = ["https://api.thecatapi.com/v1/images/search?category_ids=5&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=15&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=1&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=14&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=2&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=4&limit=5", "https://api.thecatapi.com/v1/images/search?category_ids=7&limit=5", "https://api.thecatapi.com/v1/images/search?mime_types=gif&limit=5"];
-var breeds= [];
-
-
+const butoane = [
+  buton1,
+  buton2,
+  buton3,
+  buton4,
+  buton5,
+  buton6,
+  buton7,
+  buton8
+];
+const login = {
+  headers: { "x-api-key": "7c59bf11-7fe0-4615-9ba0-327d59f34266" }
+};
+const adrese = [
+  "https://api.thecatapi.com/v1/images/search?category_ids=5&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=15&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=1&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=14&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=2&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=4&limit=5",
+  "https://api.thecatapi.com/v1/images/search?category_ids=7&limit=5",
+  "https://api.thecatapi.com/v1/images/search?mime_types=gif&limit=5"
+];
+var breeds = [];
 
 poz.style = "width:50%";
 card.style = "width:50%";
@@ -26,82 +44,86 @@ generateImage("https://www.kimballstock.com/pix/COU/01/COU_01_DB0012_01_P.JPG");
 
 card.appendChild(img);
 function generateImage(x) {
-    img.src = x;
-    img.style = "width:100% "
+  img.src = x;
+  img.style = "width:100% ";
 }
 
 function generateImage1(y) {
-    var img = document.createElement("img");
-    img.src = y;
-    poz.appendChild(img);
-    img.style = "width:45%"
+  var img = document.createElement("img");
+  img.src = y;
+  poz.appendChild(img);
+  img.style = "width:45%";
 }
 
-
 function lista(pisica) {
-    for (i = 0; i < pisica.length; i++) {
-        var lista = document.createElement('option');
-        lista.value = pisica[i].id;
-        lista.innerText = pisica[i].name;
-        select.appendChild(lista);
-    }
+  for (i = 0; i < pisica.length; i++) {
+    var lista = document.createElement("option");
+    lista.value = pisica[i].id;
+    lista.innerText = pisica[i].name;
+    select.appendChild(lista);
+  }
 }
 
 var newVar = fetch("https://api.thecatapi.com/v1/breeds")
-    .then((response) => response.json())
-    .then((data) => {
-        breeds=data;
-        lista(data)});
+  .then(response => response.json())
+  .then(data => {
+    breeds = data;
+    lista(data);
+  });
 
-select.addEventListener("change", (e) => {
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`, login)
-        .then(res => res.json())
-        .then(data => generateImage(data[0].url)
-        );
+select.addEventListener("change", e => {
+  fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`,
+    login
+  )
+    .then(res => res.json())
+    .then(data => generateImage(data[0].url));
 });
 
-card.addEventListener("click", (e) => {
-    e.preventDefault();
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`, login)
-        .then(res => res.json())
-        .then(data => {
-            if (img.src == generateImage(data[0].url)) { generateImage(data[0].url) }
-        });
+card.addEventListener("click", e => {
+  e.preventDefault();
+  fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`,
+    login
+  )
+    .then(res => res.json())
+    .then(data => {
+      if (img.src == generateImage(data[0].url)) {
+        generateImage(data[0].url);
+      }
+    });
 });
 
 function butons(but, adresa) {
-    but.addEventListener('click', (e) => {
-        poz.innerHTML = "";
-        fetch(adresa, login)
-            .then(res => res.json())
-            .then(data => {
-                for (var i = 0; i < 6; i++) { generateImage1(data[i].url) }
-            });
-    })
-};
-
-for (i = 0; i < 9; i++) {
-    butons(butoane[i], adrese[i]);
-};
-
-
-
-    
-
-pisicute.addEventListener('keyup', (e) => {
-    
-    searchedBreeds = breeds.filter(item => item.name.indexOf(pisicute.value) !== -1).map(item=> fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${item.id}`, login)).then(res=>res.json())
-    
-
-    
-    Promise.all(searchedBreeds).then(data=> console.log(data))
-    
-    
+  but.addEventListener("click", e => {
+    poz.innerHTML = "";
+    fetch(adresa, login)
+      .then(res => res.json())
+      .then(data => {
+        for (var i = 0; i < 6; i++) {
+          generateImage1(data[i].url);
+        }
+      });
+  });
 }
 
+// for (i = 0; i < 9; i++) {
+//     butons(butoane[i], adrese[i]);
+// };
 
+pisicute.addEventListener("keyup", e => {
+  poz1.innerHTML = "";
+  searchedBreeds = breeds
+    .filter(
+      item =>
+        item.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1
+    )
+    .map(item =>
+      fetch(
+        `https://api.thecatapi.com/v1/images/search?breed_ids=${item.id}&limit=4`,
+        login
+      ).then(res => res.json())
+    );
 
-
-)
-
-
+  Promise.all(searchedBreeds).then(data =>console.log(data));
+});
